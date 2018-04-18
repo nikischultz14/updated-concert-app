7e6c0c1e5ae83d69855acc2b935146e7ada3d5ea
nikischultz14 <nikischultz14@gmail.com>
2018-04-18 10:53:03 -0500

    Initial commit

---
+++ src/main/java/com/nikischultz/concertapp/ConcertAppApplication.java
@@ -0,0 +1,12 @@
+package com.nikischultz.concertapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/nikischultz/concertapp/ConcertAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.nikischultz.concertapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConcertAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
