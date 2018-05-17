325c2e1e732ee7fbf48db1f158044a4488395df6
nikischultz14 <nikischultz14@gmail.com>
2018-05-17 10:15:05 -0500

    update 1

---
+++ src/main/frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['../frontend/src/app/app.component.css']
+})
+export class AppComponent {
+  title = 'Concert App';
+}
